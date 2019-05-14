af77016754fa45a80a3afac8591f6a596b23c84f
franciscoD1202 <36548559+franciscoD1202@users.noreply.github.com>
2019-05-14 11:20:37 -0500

    Revert "Add files via upload"
    This reverts commit 849cee39572470c59bd59607ede37f8c517d0093.

--- robot_control_sim.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source /opt/ros/kinetic/setup.bash
-source /home/robotica/catkin_ws/devel/setup.bash
-roslaunch slam_sim_launch.xml
--- robot_guide.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source /opt/ros/kinetic/setup.bash
-source /home/catkin_ws/setup.bash
-
-sudo usermod -a -G dialout $USER
-sudo chmod a+rw /dev/ttyUSB0
-sudo chmod a+rw /dev/ttyUSB1
-
- roslaunch robot_control robot_config_test.launch
