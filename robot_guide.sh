93360f6930b1beed685eb11092005321aaee1bf7
Unknown <fmj.durango10@uniandes.edu.co>
2019-05-23 12:14:08 -0500

    Revision 23/05/2019
    Revised publishing speeds

---
+++ robot_guide.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+source /opt/ros/kinetic/setup.bash
+source /home/francisco/catkin_ws/devel/setup.bash
+
+sudo usermod -a -G dialout $USER
+sudo chmod a+rw /dev/ttyUSB0
+sudo chmod a+rw /dev/ttyUSB1
+
+ROS_MASTER_URI=http://157.253.202.241:11311
