54dcf7b4cfaab38404095cd9d3bf9638e2167ea6
Unknown <fmj.durango10@uniandes.edu.co>
2019-05-26 15:42:09 -0500

    Version 0.2
    return to previous version (Robot guide)

--- robot_guide.sh
+++ robot_guide.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-systemctl start vncserver-x11-serviced.service D
+
 echo "Start..." 
 source /opt/ros/kinetic/setup.bash
 . /home/francisco/catkin_ws/devel/setup.bash
@@ -9,8 +9,8 @@ sudo usermod -a -G dialout $USER
 sudo chmod a+rw /dev/ttyUSB0
 sudo chmod a+rw /dev/ttyUSB1
 
-export ROS_MASTER_URI=http://157.253.202.241:11311
-ROS_MASTER_URI=http://157.253.202.241:11311
+export ROS_MASTER_URI=http://157.253.207.241:11311
+ROS_MASTER_URI=http://157.253.207.241:11311
 
-echo "Set up completed, press Ctrl+C"
-echo "run roslaunch  robot_config_test from robot_control"
+echo "Set up completed"
+echo "Launching ROBOT GUIDE"
