5e7e93888252f5270d905a76eb2279f22c1f8234
Unknown <fmj.durango10@uniandes.edu.co>
2019-05-26 13:02:26 -0500

    Update 26/05/2019
    Update robot_guide.sh

--- robot_guide.sh
+++ robot_guide.sh
@@ -2,13 +2,14 @@
 clear
 echo "Start..." 
 source /opt/ros/kinetic/setup.bash
-xterm -hold -e "source /home/francisco/catkin_ws/devel/setup.bash"
+. /home/francisco/catkin_ws/devel/setup.bash
 
 sudo usermod -a -G dialout $USER
 sudo chmod a+rw /dev/ttyUSB0
 sudo chmod a+rw /dev/ttyUSB1
 
-ROS_MASTER_URI=http://157.253.202.241:11311
 export ROS_MASTER_URI=http://157.253.202.241:11311
+ROS_MASTER_URI=http://157.253.202.241:11311
 
-echo "Completed..."
+echo "Set up completed, press Ctrl+C"
+echo "run roslaunch  robot_config_test from robot_control"
