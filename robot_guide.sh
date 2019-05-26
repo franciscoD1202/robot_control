61d6a7f632abee8b5ea18156e60599f0c0a641fc
franciscoD1202 <36548559+franciscoD1202@users.noreply.github.com>
2019-05-26 15:44:58 -0500

    Update robot_guide.sh

--- robot_guide.sh
+++ robot_guide.sh
@@ -9,8 +9,8 @@ sudo usermod -a -G dialout $USER
 sudo chmod a+rw /dev/ttyUSB0
 sudo chmod a+rw /dev/ttyUSB1
 
-export ROS_MASTER_URI=http://157.253.207.241:11311
 ROS_MASTER_URI=http://157.253.207.241:11311
+export ROS_MASTER_URI=http://157.253.207.241:11311
 
 echo "Set up completed"
 echo "Launching ROBOT GUIDE"
